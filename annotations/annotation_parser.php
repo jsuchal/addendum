e1f2e9659d26c615f990b458f01e139b81b32ec6
jan.suchal <jan.suchal@fa2e90a8-c81d-0410-b55e-9bccfdb07dd3>
2009-12-18 12:30:38 +0000

    Added support for null values.

--- annotations/annotation_parser.php
+++ annotations/annotation_parser.php
@@ -187,6 +187,8 @@
 			$this->add(new ConstantMatcher('false', false));
 			$this->add(new ConstantMatcher('TRUE', true));
 			$this->add(new ConstantMatcher('FALSE', false));
+			$this->add(new ConstantMatcher('NULL', null));
+			$this->add(new ConstantMatcher('null', null));
 			$this->add(new AnnotationStringMatcher);
 			$this->add(new AnnotationNumberMatcher);
 			$this->add(new AnnotationArrayMatcher);
